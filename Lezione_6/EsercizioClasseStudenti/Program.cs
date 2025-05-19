46e3afe9b1b0769eb8b45ff09cdc239f3ccf85b4
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-19 17:38:30 +0200

    Add files via upload

---
+++ Lezione_6/EsercizioClasseStudenti/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+public class Studente
+{
+    // Definiamo le proprietà dello Studente 
+    public string Nome;
+    public int Matricola;
+    public double MediaVoti;
+
+    /// <summary>
+    /// Costruttore che riceve e assegna le tre proprietà
+    /// </summary>
+    /// <param name="nome"></param>
+    /// <param name="matricola"></param>
+    /// <param name="mediaVoti"></param>
+    public Studente(string nome, int matricola, double mediaVoti)
+    {
+        Nome = nome;
+        Matricola = matricola;
+        MediaVoti = mediaVoti;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Definizione delll'array studente 
+        Studente[] studenti = new Studente[2];
+
+        //Ciclo che richiede nomi matricole e mediaVoti per inserire singolarmente ogni studente in un array di studenti
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine("Inserisci il nome dello studente: ");
+            string? nome = Console.ReadLine();
+
+            Console.WriteLine("Inserisci la matricola dello studente: ");
+            int matricola = TryInputIntero();
+
+            Console.WriteLine("Inserisci la media voti dello studente: ");
+            double mediaVoti = TryInputDouble();
+
+            studenti[i] = new Studente(nome, matricola, mediaVoti);
+
+            StampaStudente(studenti[i], i);
+        }
+
+    }
+
+    /// <summary>
+    /// Stampa il numero dello studente nell'array con il suo nome matricola e media voti
+    /// </summary>
+    /// <param name="studente"></param>
+    /// <param name="i"></param>
+    public static void StampaStudente(Studente studente, int i)
+    {
+        Console.WriteLine($"Lo Studente{i + 1} si chiama {studente.Nome}, la sua matricola è {studente.Matricola} e la sua media voti è {studente.MediaVoti}");
+    }
+
+    /// <summary>
+    /// Prende in input una stringa e la trasforma in numero intero altrimenti da errore
+    /// </summary>
+    /// <returns></returns>
+    public static int TryInputIntero()
+    {
+        bool controllo = false;
+        int numero = 0;
+
+        do
+        {
+            try
+            {
+                numero = int.Parse(Console.ReadLine());
+                controllo = true;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        } while (!controllo);
+
+        return numero;
+    }
+
+    /// <summary>
+    /// Prende in input una stringa e la trasforma in double altrimenti da errore
+    /// </summary>
+    /// <returns></returns>
+    public static double TryInputDouble()
+    {
+        bool controllo = false;
+        double numero = 0;
+
+        do
+        {
+            try
+            {
+                numero = double.Parse(Console.ReadLine());
+                controllo = true;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        } while (!controllo);
+
+        return numero;
+    }
+}
---
+++ Lezione_6/EsercizioOperazioni/Operazioni.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+public class Operazioni
+{
+    public static void Main(string[] args)
+    {
+
+        Input();
+
+    }
+
+    /// <summary>
+    /// Svolge l'operazione di Somma tra 2 numeri interi
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns>Valore della somma</returns>
+    public static int Somma(int a, int b)
+    {
+        return a + b;
+    }
+
+    /// <summary>
+    /// Svolge la Moltiplicazione fra due numeri interi
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns>Valore della moltiplicazione</returns>
+    public static int Moltiplica(int a, int b)
+    {
+        return a * b;
+    }
+
+    /// <summary>
+    /// Svolge la divisione tra due interi con il controllo per la divisione per 0
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns>valore divisione</returns>
+    public static object Divisione(int a, int b)
+    {
+        try
+        {
+            return a / b;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+            return "Non valido";
+        }
+    }
+
+    /// <summary>
+    /// Metodo che Stampa il risultato dell'operazione data
+    /// </summary>
+    /// <param name="operazione"></param>
+    /// <param name="risultato"></param>
+    public static void StampaRisultato(string operazione, object risultato)
+    {
+        Console.WriteLine($"Il risultato dell'operazione {operazione} è: {risultato}");
+    }
+
+    /// <summary>
+    /// Funzione di input che riceve due interi e l'intero per la scelta con controllo interno, 
+    /// inoltre richiama la funzione di stampa del risultato in base alla scelta fatta dall' operatore
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    public static void Input()
+    {
+        Console.WriteLine("Inserisci il primo numero: ");
+        int a = TryInputNumero();
+
+        Console.WriteLine("Inserisci il secondo numero: ");
+        int b = TryInputNumero();
+
+        Console.WriteLine("Inserisci l'operazione che vuoi svolgere (1(somma), 2(moltiplicazione), 3(divisione)): ");
+        int scelta = TryInputNumero();
+
+        if (scelta == 1)
+        {
+            StampaRisultato("Somma", Somma(a, b));
+        }
+        else if (scelta == 2)
+        {
+            StampaRisultato("Moltiplicazione", Moltiplica(a, b));
+        }
+        else if (scelta == 3)
+        {
+            StampaRisultato("Divisione", Divisione(a, b));
+        }
+    }
+
+    /// <summary>
+    /// Metodo che controlla se in input viene inserito un numero valido
+    /// </summary>
+    /// <returns>Il numero se il risultato è valido, altrimenti errore</returns>
+    public static int TryInputNumero()
+    {
+        bool controllo = false;
+        int numero = 0;
+
+        do
+        {
+            try
+            {
+                numero = int.Parse(Console.ReadLine());
+                controllo = true;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        } while (!controllo);
+
+        return numero;
+    }
+}
