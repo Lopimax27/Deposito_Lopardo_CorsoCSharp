bf3afc872cf2dba25df96cd2ccf3beb10ff57516
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-19 17:34:56 +0200

    Add files via upload

---
+++ Lezione_1/MyProject/Program.cs
@@ -0,0 +1,62 @@
+﻿/* using System;
+
+class Program
+{
+    static int contatore=10;
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World");  //Messaggio in inglese 
+        contatore=45;
+        Console.WriteLine(contatore);    //Messaggio in italiano
+    MyMethod();
+    Case Sensitive sulle variabili
+    int eta=45;
+    int Eta=87;
+    Console.WriteLine(eta);
+    Console.WriteLine(Eta);
+    var myvar ="ciao"; Non sei pazzesca ma potresti essere utile con variabili molto lunghe o complesse
+    
+    TipiDiDato();
+    }
+    /// <summary>
+    /// questo è il primo metodo 
+    /// </summary>
+    static void MyMethod(){}
+    static void TipiDiDato()
+    {
+        /*
+        int numero = 10;    //numero intero
+        float decimale = 3.14f; //numero a virgola mobile
+        char lettera = 'A'; //carattere
+        bool condizione = true; //booleano 0 false e 1 true
+        string saluto = "Ciao a tutti"; //stringa di caratteri
+
+        Console.WriteLine("Numero"+numero);
+        Console.WriteLine($"Decimale:{decimale}");
+        Console.WriteLine($"Lettera:{lettera}");
+        Console.WriteLine($"Condizione: {condizione}");
+        Console.WriteLine($"Saluto: {saluto}");
+        
+        const solo tipi primitivi, invece readonly puoi inserirci anche classi scritte dall utente 
+
+        casting implicito float=
+
+        Console.Write("Ciao inserisci nome: ");
+        string? nome=Console.ReadLine();
+        Console.WriteLine($"Ciao {nome}");
+
+        Console.Write("Dammi un numero intero:");
+        string? n1=Console.ReadLine();
+
+        Console.Write("Dammi un altro numero intero:");
+        string? n2=Console.ReadLine();
+        
+        int numero1 = int.Parse(n1);
+        int numero2 = int.Parse(n2);
+
+        Console.WriteLine($"Somma:{numero1+numero2}");
+
+
+    }
+}
+*/
