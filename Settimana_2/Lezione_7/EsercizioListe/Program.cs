d38d615e2f641cd88636e468c83fd354573ff50d
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-20 17:31:42 +0200

    Upload
    Add:
    +Commenti in file EsercizioListe

--- Settimana_2/Lezione_7/EsercizioListe/Program.cs
+++ Settimana_2/Lezione_7/EsercizioListe/Program.cs
@@ -4,19 +4,22 @@ public class Program
 {
     public static void Main()
     {
+        //Creazione della lista della spesa, Messaggio di richiesta e inserimento del numero di CDC
         List<string> listaSpesa = new List<string>();
         Console.WriteLine("Quante cose devi comprare?");
         int CDComprare = int.Parse(Console.ReadLine());
 
         for (int i = 0; i < CDComprare; i++)
-        {
+        {   
+            //Aggiunge elemento ricevuto in input alla lista della Spesa
             Console.Write("Inserisci una cosa da comprare al supermercato: ");
             listaSpesa.Add(Console.ReadLine());
         }
 
         for (int i = 0; i < CDComprare; i++)
         {
-            Console.WriteLine($"Elemento {i+1} = {listaSpesa[i]}");
+            //Stampa dei valori
+            Console.WriteLine($"Elemento {i + 1} = {listaSpesa[i]}");
         }
     }
 }
