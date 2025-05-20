a36d7f9f45ce73caddaa3465c78e168300b23f9a
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-20 17:20:22 +0200

    Upload
    upload:
    +EsercizioListe

---
+++ Settimana_2/Lezione_7/EsercizioListe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<string> listaSpesa = new List<string>();
+        Console.WriteLine("Quante cose devi comprare?");
+        int CDComprare = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < CDComprare; i++)
+        {
+            Console.Write("Inserisci una cosa da comprare al supermercato: ");
+            listaSpesa.Add(Console.ReadLine());
+        }
+
+        for (int i = 0; i < CDComprare; i++)
+        {
+            Console.WriteLine($"Elemento {i+1} = {listaSpesa[i]}");
+        }
+    }
+}
