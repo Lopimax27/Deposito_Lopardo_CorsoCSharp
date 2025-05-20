745d5542b7f2e3d785500d0baa14b106fecf2c47
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-20 17:02:05 +0200

    Upload esercizio array

---
+++ Settimana_2/Lezione_7/EsercizioArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //definizione di array nomi e voti
+        int[] voti = new int[5];
+        string[] nomi = new string[5];
+
+        //Ciclo che assegna un valore di nome e voto ad ogni iterazione ad ogni elemento degli array
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Inserisci il tuo nome: ");
+            nomi[i] = Console.ReadLine();
+            Console.Write($"Inserisici il voto di {nomi[i]}: ");
+            voti[i] = int.Parse(Console.ReadLine());
+        }
+
+        //Ciclo per Stampare Nome:Voto
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{nomi[i]}:{voti[i]}");
+        }
+
+        //Stampa grazie ad Average, Max e Min stampa i valori con piccola descrizione
+        Console.WriteLine($"La media vale {voti.Average()}, il valore max {voti.Max()}, il valore min {voti.Min()}");
+    }
+
+}
