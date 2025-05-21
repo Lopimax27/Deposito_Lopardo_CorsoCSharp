0a7eb45941e6b1c5584fa307dc9cd427e50f713a
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-21 11:04:39 +0200

    EsercizioVIdeoteca
    inserimento esercizio senza extra

---
+++ Settimana_2/Lezione_8/VideotecaDigitale/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+public class Film
+{
+    public string titolo, regista, genere;
+    public int anno;
+
+    public Film(string tit, string reg, string gen, int ann)
+    {
+        titolo = tit;
+        regista = reg;
+        genere = gen;
+        anno = ann;
+    }
+
+    public override string ToString()
+    {
+        return $"Il film {titolo} di {regista} dell'anno {anno}, genere {genere}";
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Film> videoteca = new List<Film>();
+
+        Console.WriteLine("Benvenuto nella videoteca!");
+        bool controllore1 = true;
+
+        do
+        {
+            Console.WriteLine("Eccoti nel menu: Premere 1 per aggiungere un film alla raccolta, premere 2 per scegliere un genere e visualizzare i film proposti, premere 3 per visualizzare tutta la raccolta e 4 per uscire");
+            int scelta = int.Parse(Console.ReadLine());
+
+            switch (scelta)
+            {
+                case 1:
+                    videoteca.Add(InputUtente());
+                    break;
+                case 2:
+                    SceltaGenere(videoteca);
+                    break;
+                case 3:
+                    Stampa(videoteca);
+                    break;
+                case 4:
+                    controllore1 = false;
+                    break;
+            }
+
+        } while (controllore1);
+
+        Console.WriteLine("Arrivederci e buon film!");
+
+    }
+
+    public static Film InputUtente()
+    {
+        Console.Write("Inserisci il titolo del film: ");
+        string titolo = Console.ReadLine();
+
+        Console.Write("Inserisci il regista del film: ");
+        string regista = Console.ReadLine();
+
+        Console.Write("Inserisci il genere del film: ");
+        string genere = Console.ReadLine();
+
+        Console.Write("Inserisci l'anno di uscita del film: ");
+        int anno = int.Parse(Console.ReadLine());
+
+        return new Film(titolo, regista, genere, anno);
+    }
+
+    public static void Stampa(List<Film> videoteca)
+    {
+        Console.WriteLine("Nella raccolta abbiamo:");
+        foreach (Film film in videoteca)
+        {
+            Console.WriteLine(film);
+        }
+    }
+
+    public static void SceltaGenere( List<Film> videoteca)
+    {
+        int conto = 0;
+        Console.Write("Inserisci il genere del film che vuoi vedere: ");
+        string genere = Console.ReadLine();
+
+        foreach (Film film in videoteca)
+        {
+            if (film.genere == genere)
+            {
+                Console.WriteLine(film);
+                conto++;
+            }
+        }
+        if (conto == 00)
+        {
+            Console.WriteLine("Non abbiamo film di questo genere, riprovare o aggiungerne uno");
+        }
+    }
+
+}
