018406a5e0a5f0371b7ef91e543b14bf31dd62e9
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-21 12:46:00 +0200

    Upload Esercizio Videoteca e EsempiEreditarietà
    Add:
    +Commenti e spiegazioni del codice in VideotecaDigitale

---
+++ Settimana_2/Lezione_8/EsempiEreditarietà/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+//Classe base
+public class Animale
+{
+    public virtual void FaiVerso()
+    {
+        Console.WriteLine("L'animale fa un verso.");
+    }
+}
+
+//Classe derivata 
+public class Cane : Animale
+{
+    //Metodo specifico della classe derivata
+    public void Scondinzola()
+    {
+        Console.WriteLine("Il cane scodinzola.");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Cane mioCane = new Cane();
+        mioCane.FaiVerso();
+        mioCane.Scondinzola();
+    }
+}
--- Settimana_2/Lezione_8/VideotecaDigitale/Program.cs
+++ Settimana_2/Lezione_8/VideotecaDigitale/Program.cs
@@ -2,9 +2,17 @@
 
 public class Film
 {
+    //Definizione di proprietà titolo regista genere e anno
     public string titolo, regista, genere;
     public int anno;
 
+    /// <summary>
+    /// Generazione del costruttore
+    /// </summary>
+    /// <param name="tit"></param>
+    /// <param name="reg"></param>
+    /// <param name="gen"></param>
+    /// <param name="ann"></param>
     public Film(string tit, string reg, string gen, int ann)
     {
         titolo = tit;
@@ -13,6 +21,10 @@ public class Film
         anno = ann;
     }
 
+    /// <summary>
+    /// Override della funzione ToString per stampare la descrizione del film
+    /// </summary>
+    /// <returns></returns>
     public override string ToString()
     {
         return $"Il film {titolo} di {regista} dell'anno {anno}, genere {genere} è presente in raccolta";
@@ -23,29 +35,32 @@ public class Program
 {
     public static void Main()
     {
-        List<Film> videoteca = new List<Film>();
+        List<Film> videoteca = new List<Film>(); //inizializzazione lista videoteca
 
         Console.WriteLine("Benvenuto nella videoteca!");
+        //Controllore1  variabile che fa continuare il ciclo while fino a quando l'utente richiede di uscire dal programma 
         bool controllore1 = true;
 
         do
         {
-            Console.WriteLine("Eccoti nel menu: Premere 1 per aggiungere un film alla raccolta, premere 2 per scegliere un genere e visualizzare i film proposti, premere 3 per visualizzare tutta la raccolta e 4 per uscire");
+            //Menu visualizzato
+            Console.WriteLine("Eccoti nel menu: Premere 1 per aggiungere un film alla raccolta, premere 2 per effettuare una ricerca nella raccolta, premere 3 per visualizzare tutta la raccolta e 4 per uscire");
             int scelta = int.Parse(Console.ReadLine());
 
+            //switch per muoversi nel menu e scegliere cosa fare e che funzioni richiamare
             switch (scelta)
             {
                 case 1:
-                    videoteca.Add(InputUtente());
+                    videoteca.Add(InputUtente());//Aggiunge il film restituito da InputUtente
                     break;
                 case 2:
-                    RicercaFilm(videoteca);
+                    RicercaFilm(videoteca);//Entra nello switch per scegliere in base a cosa cercare
                     break;
                 case 3:
-                    Stampa(videoteca);
+                    Stampa(videoteca);//Richiama la funzione di stampa di tutti i film in raccolta
                     break;
                 case 4:
-                    controllore1 = false;
+                    controllore1 = false;//booleano che settato false chiude il ciclo
                     break;
             }
 
@@ -55,6 +70,10 @@ public class Program
 
     }
 
+    /// <summary>
+    /// Richiede all'utente i dati del film
+    /// </summary>
+    /// <returns>Il nuovo film per salvarlo nella lista</returns>
     public static Film InputUtente()
     {
         Console.Write("Inserisci il titolo del film: ");
@@ -72,6 +91,10 @@ public class Program
         return new Film(titolo, regista, genere, anno);
     }
 
+    /// <summary>
+    /// Grazie al ciclo stampa tutti gli elementi della lista
+    /// </summary>
+    /// <param name="videoteca"></param>
     public static void Stampa(List<Film> videoteca)
     {
         Console.WriteLine("Nella raccolta abbiamo:");
@@ -81,11 +104,16 @@ public class Program
         }
     }
 
+    /// <summary>
+    /// Funzione di switch che permette di scegliere il criterio di ricerca 
+    /// </summary>
+    /// <param name="videoteca"></param>
     public static void RicercaFilm(List<Film> videoteca)
     {
         Console.Write("Come vuoi cercare il film (1.titolo,2.regista,3.genere,4.anno) ?: ");
         int ricerca = int.Parse(Console.ReadLine());
 
+        //Richiama la funzione in base alla scelta dell'utente
         switch (ricerca)
         {
             case 1:
@@ -105,6 +133,10 @@ public class Program
         }
     }
 
+    /// <summary>
+    /// Funzione che imposta il controllo per ogni film in base al genere e conta i presenti in caso messaggio di errore
+    /// </summary>
+    /// <param name="videoteca"></param>
     public static void SceltaGenere(List<Film> videoteca)
     {
         int conto = 0;
@@ -113,7 +145,7 @@ public class Program
 
         foreach (Film film in videoteca)
         {
-            if (film.genere == genere)
+            if (film.genere == genere) //verifica se il genere coincide con quello preso in i
             {
                 Console.WriteLine(film);
                 conto++;
@@ -125,6 +157,10 @@ public class Program
         }
     }
 
+    /// <summary>
+    /// Funzione che imposta il controllo per ogni film in base al titolo e conta i presenti in caso messaggio di errore
+    /// </summary>
+    /// <param name="videoteca"></param>
     public static void SceltaTitolo(List<Film> videoteca)
     {
         int conto = 0;
@@ -145,6 +181,10 @@ public class Program
         }
     }
 
+    /// <summary>
+    /// Funzione che imposta il controllo per ogni film in base al regista e conta i presenti in caso messaggio di errore
+    /// </summary>
+    /// <param name="videoteca"></param>
     public static void SceltaRegista(List<Film> videoteca)
     {
         int conto = 0;
@@ -165,7 +205,11 @@ public class Program
         }
     }
     
-    public static void SceltaAnno( List<Film> videoteca)
+    /// <summary>
+    /// Funzione che imposta il controllo per ogni film in base all'anno e conta i presenti in caso messaggio di errore
+    /// </summary>
+    /// <param name="videoteca"></param>
+    public static void SceltaAnno(List<Film> videoteca)
     {
         int conto = 0;
         Console.Write("Inserisci il titolo del film che vuoi vedere: ");
