9f98714939b6aeba82f73f10dc84be043f8b01bf
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-22 12:46:52 +0200

    Incapsulamento
    Add:
    +Esempio Base
    +Incapsulamento Viaggio Prenotazione

---
+++ Settimana_2/Lezione_9/EsempiIncapsulamento/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class ContoBancario
+{
+    //Campo privato NON ACCESSIBILE DIRETTAMENTE DALL'ESTERNO
+    private double _saldo;
+
+    //Proprietà per accedere al saldo in modo Controllato
+    public double Saldo
+    {
+        get
+        {
+            return _saldo;       //permette la lettura del saldo
+        }
+        set
+        {
+            if (value >= 0)     //solo valori validi
+            {
+                _saldo = value;
+            }
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        ContoBancario conto = new ContoBancario();
+
+        conto.Saldo = 1000.50;              //imposta il saldo tramite la proprietà
+        Console.WriteLine(conto.Saldo);     //legge il saldo tramite la proprietà restituisce 1000.50
+
+        conto.Saldo = -500;                 //non modifica il saldo che è negativo
+        Console.WriteLine(conto.Saldo);     // rimane 1000.50
+    }
+}
---
+++ Settimana_2/Lezione_9/PrenotaViaggio/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+public class PrenotazioneViaggio
+{
+    public const int maxPosti = 20;
+    private int _postiPrenotati, _postiDisponibili;
+    private string _destinazione;
+
+    public string Destinazione
+    {
+        get
+        {
+            return _destinazione;
+        }
+        set
+        {
+            _destinazione=value;
+        }
+    }
+    public int PostiPrenotati
+    {
+        get
+        {
+            return _postiPrenotati;
+        }
+    }
+    public int PostiDisponibili
+    {
+        get
+        {
+            return maxPosti - _postiPrenotati;
+        }
+    }
+    public void PrenotaPosti(int numero)
+    {
+        if (numero <= PostiDisponibili)
+        {
+            _postiPrenotati += numero;
+        }
+    }
+
+    public void AnnullaPrenotazione(int numero)
+    {
+        if (numero > 0 && numero<=PostiPrenotati)
+        {
+            _postiPrenotati -= numero;
+        }
+        else
+        {
+            Console.WriteLine("Numero inserito non valido");
+        }
+    }
+
+    public PrenotazioneViaggio(int postiP, string dest)
+    {
+        _postiDisponibili = maxPosti;
+        _postiPrenotati = postiP;
+        _destinazione = dest;
+    }
+    
+    public override string ToString()
+    {
+        return $"Destinazione: {Destinazione} Posti Prenotati:{PostiPrenotati} Posti Disponibili: {PostiDisponibili}";
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        PrenotazioneViaggio Viaggio1 = new PrenotazioneViaggio(0, "");
+
+        Console.Write("Inserisci la destinazione del tuo viaggio: ");
+        Viaggio1.Destinazione = Console.ReadLine();
+
+        Console.Write("Inserisci quante persone vogliono prenotare il viaggio: ");
+        int numP = int.Parse(Console.ReadLine());
+        Viaggio1.PrenotaPosti(numP);
+
+        Console.Write("Quante persone vogliono annullare la prenotazione: ");
+        int numA = int.Parse(Console.ReadLine());
+        Viaggio1.AnnullaPrenotazione(numA);
+
+        Console.WriteLine(Viaggio1);
+    }
+}
