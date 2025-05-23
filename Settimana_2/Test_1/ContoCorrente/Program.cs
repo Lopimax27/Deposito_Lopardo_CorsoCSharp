b2983e983f2354e4483066c918ccd4c851551cf6
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-23 16:23:36 +0200

    Inserimento di Commenti
    Esercizio test veicoli e conto per completezza

--- Settimana_2/Test_1/ContoCorrente/Program.cs
+++ Settimana_2/Test_1/ContoCorrente/Program.cs
@@ -2,9 +2,11 @@
 
 public class ContoCorrente
 {
+    //Definizione di due campi private saldo e num operazioni
     private decimal _saldo;
     private int _numeroOperazioni;
 
+    //Proprietà del saldo di sola lettura
     public decimal Saldo
     {
         get
@@ -13,6 +15,7 @@ public class ContoCorrente
         }
     }
 
+    //Proprietà del numero operazioni di sola lettura
     public int NumeroOperazioni
     {
         get
@@ -21,18 +24,29 @@ public class ContoCorrente
         }
     }
 
+    /// <summary>
+    /// Metodo versa che modifica il numero di operazioni e il saldo in base all'importo
+    /// </summary>
+    /// <param name="importo"></param>
     public void Versa(decimal importo)
     {
         _saldo += importo;
         _numeroOperazioni++;
     }
 
+    /// <summary>
+    /// Metodo preleva che modifica il numero di operazioni e il saldo in base all'importo da prelevare
+    /// </summary>
+    /// <param name="importo"></param>
     public void Preleva(decimal importo)
     {
         _saldo -= importo;
         _numeroOperazioni++;
     }
 
+    /// <summary>
+    /// Visualizza il saldo con il numero di operazioni
+    /// </summary>
     public void VisualizzaSaldo()
     {
         Console.WriteLine($"Saldo disponibile:{Saldo} e numero di operazioni: {NumeroOperazioni}");
@@ -43,26 +57,31 @@ public class Program
 {
     public static void Main()
     {
+        //creazione del conto corrente
         ContoCorrente mioConto = new ContoCorrente();
-        bool x = true;
+        bool x = true; // variabile booleana impostata a 3 che viene impostata a falso quando utente chiede di uscire
         do
         {
+            //Visualizzazione del Menu
             Console.WriteLine("1.Preleva dal Conto\n2.Versa sul Conto\n3.Visualizza saldo\n0.Esci");
             int scelta = int.Parse(Console.ReadLine());
 
             switch (scelta)
             {
                 case 1:
+                    //Prelevare l'importo dato in input e richiama il metodo preleva
                     Console.WriteLine("Inserisci l'importo da prelevare: ");
                     decimal importo1 = decimal.Parse(Console.ReadLine());
                     mioConto.Preleva(importo1);
                     break;
                 case 2:
+                    //Versare l'importo ricevuto in input e richiama la funzione versa
                     Console.WriteLine("Inserisci l'importo da versare: ");
                     decimal importo2 = decimal.Parse(Console.ReadLine());
                     mioConto.Versa(importo2);
                     break;
                 case 3:
+                    //Visualizza il conto con le operazioni eseguite e il saldo
                     mioConto.VisualizzaSaldo();
                     break;
                 case 0:
--- Settimana_2/Test_1/Veicoli/Program.cs
+++ Settimana_2/Test_1/Veicoli/Program.cs
@@ -2,9 +2,16 @@
 
 public class Veicolo
 {
+    //Campi privati della classe base marca modello
     public string marca, modello;
     public int annoImmatricolazione;
 
+    /// <summary>
+    /// Costruttore Base predefinito
+    /// </summary>
+    /// <param name="ma"></param>
+    /// <param name="mo"></param>
+    /// <param name="a"></param>
     public Veicolo(string ma, string mo, int a)
     {
         marca = ma;
@@ -12,6 +19,9 @@ public class Veicolo
         annoImmatricolazione = a;
     }
 
+    /// <summary>
+    /// Metodo Stampa info per stampare la descrizione del veicolo
+    /// </summary>
     public virtual void StampaInfo()
     {
         Console.Write($"Marca: {marca} Modello: {modello} AnnoImmatricolazione:{annoImmatricolazione}");
@@ -20,9 +30,18 @@ public class Veicolo
 
 public class AutoAziendale : Veicolo
 {
+    //campi publici identificativi dell'autoaziendale
     public string targa;
     public bool usoPrivato;
 
+    /// <summary>
+    /// Costruttore personalizzato per auto aziendale
+    /// </summary>
+    /// <param name="ma"></param>
+    /// <param name="mo"></param>
+    /// <param name="a"></param>
+    /// <param name="ta"></param>
+    /// <param name="usP"></param>
     public AutoAziendale(string ma, string mo, int a, string ta, bool usP)
     : base(ma, mo, a)
     {
@@ -33,17 +52,28 @@ public class AutoAziendale : Veicolo
         usoPrivato = usP;
     }
 
+    /// <summary>
+    /// Override dello stampainfo base per stampare anche targa e uso privato
+    /// </summary>
     public override void StampaInfo()
     {
         base.StampaInfo();
-        Console.Write($"Targa: {targa} UsoPrivato: {usoPrivato}");
+        Console.WriteLine($"Targa: {targa} UsoPrivato: {usoPrivato}");
     }
 }
 
 public class FurgoneAziendale : Veicolo
 {
+    //campo publico della capacità carico
     public int capacitàCarico;
 
+    /// <summary>
+    /// Costruttore personalizzato dei furgoni aziendali 
+    /// </summary>
+    /// <param name="ma"></param>
+    /// <param name="mo"></param>
+    /// <param name="a"></param>
+    /// <param name="cC"></param>
     public FurgoneAziendale(string ma, string mo, int a, int cC)
     : base(ma, mo, a)
     {
@@ -53,10 +83,13 @@ public class FurgoneAziendale : Veicolo
         capacitàCarico = cC;
     }
 
+    /// <summary>
+    /// override dela Stampa con descrizione completa del furgone
+    /// </summary>
     public override void StampaInfo()
     {
         base.StampaInfo();
-        Console.Write($"Capacità Carico: {capacitàCarico} kg ");
+        Console.WriteLine($"Capacità Carico: {capacitàCarico} kg ");
     }
 }
 
@@ -69,7 +102,7 @@ public class Program
 
         do
         {
-            Console.WriteLine("1.Aggiungi un auto al garage dell'azienda\n2.Aggiungi un furgone\n3.Visualizza i veicoli aziendali\n0.Esci");
+            Console.WriteLine("\n1.Aggiungi un auto al garage dell'azienda\n2.Aggiungi un furgone\n3.Visualizza i veicoli aziendali\n0.Esci");
             int scelta = int.Parse(Console.ReadLine());
 
             switch (scelta)
@@ -92,6 +125,12 @@ public class Program
 
     }
 
+    /// <summary>
+    /// Input Veicolo che si differenzia in base alla scelta e prende in input marca modello e anno per tutti, poi targa e uso per il caso
+    /// auto aziendale e per il furgone
+    /// </summary>
+    /// <param name="scelta"></param>
+    /// <returns></returns>
     public static Veicolo InputVeicolo(int scelta)
     {
         Console.Write("Inserisci la marca del veicolo: ");
@@ -104,7 +143,7 @@ public class Program
         int anno = int.Parse(Console.ReadLine());
         bool privato;
 
-        if (scelta == 1)
+        if (scelta == 1)//Caso auto
         {
             Console.WriteLine("Inserisci la targa: ");
             string targa = Console.ReadLine();
@@ -120,16 +159,20 @@ public class Program
             }
             return new AutoAziendale(marca, modello, anno, targa, privato);
         }
-        else if (scelta == 2)
+        else if (scelta == 2)//Caso furgone
         {
             Console.Write("Inserisci la capacità di carico: ");
             int carico = int.Parse(Console.ReadLine());
             return new FurgoneAziendale(marca, modello, anno, carico);
         }
 
-        return null;
+        return null;// null per non avere errori ma in realtà non necessario perchè la funzione non richiama mai delle scelte non possibili
     }
 
+    /// <summary>
+    /// Funzione Visualizza che stampa
+    /// </summary>
+    /// <param name="veicoli"></param>
     public static void VisualizzaGarage(List<Veicolo> veicoli)
     {
         foreach (Veicolo v in veicoli)
