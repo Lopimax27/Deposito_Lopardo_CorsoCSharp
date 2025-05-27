2414eff1a1dd1de3b0c24ea8891aba8769415138
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-27 17:53:37 +0200

    Aggiunta Commenti

--- Settimana_3/Lezione_12/EsercizioSingletonLogger/Program.cs
+++ Settimana_3/Lezione_12/EsercizioSingletonLogger/Program.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 
+//Creazione del singleton Logger
 public sealed class Logger
 {
+    //Creazione dell'istanza
     private static Logger _instance;
 
+    //Campi del logger utente password e lista dei messaggi dell'utente
     private List<string> listaLog;
     private string _utente, _password;
 
+    //Costruttore che inizializza la lista vuota
     private Logger()
     {
         listaLog = new List<string>();
     }
 
+    /// <summary>
+    /// Metodo logger che controlla se l'instanza è nulla nel caso la crea 
+    /// </summary>
+    /// <returns>e la ritorna</returns>
     public static Logger GetInstance()
     {
         if (_instance == null)
@@ -21,16 +29,28 @@ public sealed class Logger
         return _instance;
     }
 
+    /// <summary>
+    /// Metodo void per creare e impostare la password e il nome utente che non sono leggibili dall'esterno e neanche impostabili se non attraverso questo metodo
+    /// </summary>
+    /// <param name="utente"></param>
+    /// <param name="password"></param>
     public void CreaUtEPsw(string utente, string password)
     {
         _utente = utente;
         _password = password;
     }
+    /// <summary>
+    /// Metodo che aggiunge alla lista il messaggio dato dall'utente e lo salva
+    /// </summary>
+    /// <param name="message"></param>
     public void Log(string message)
     {
         listaLog.Add(message);
     }
 
+    /// <summary>
+    /// Metodo void che stampa tutti gli elementi della lista
+    /// </summary>
     public void MostraLog()
     {
         foreach (string m in listaLog)
@@ -45,8 +65,10 @@ public class Program
 {
     public static void Main()
     {
+        //Primo richiamo di get instance viene creato il primo logger
         Logger log1 = Logger.GetInstance();
 
+        //imposto l'utente e la password
         Console.WriteLine("Inserisci il nome utente:");
         string utente = Console.ReadLine();
 
@@ -54,6 +76,7 @@ public class Program
         string password = Console.ReadLine();
         log1.CreaUtEPsw(utente, password);
 
+        //Prende in input i due messaggi
         Console.WriteLine("Inserisci il messaggio del log1: ");
         log1.Log(Console.ReadLine());
 
@@ -61,9 +84,11 @@ public class Program
         Console.WriteLine("Inserisci messaggio del log2: ");
         log2.Log(Console.ReadLine());
 
+        //Utilizza mostralog sulle due variabili per verificare che in realtà sono la stessa cosa e hanno la stessa lista
         log1.MostraLog();
         log2.MostraLog();
 
+        //Esegue il controllo booleano
         Console.WriteLine(log1 == log2);
 
     }
