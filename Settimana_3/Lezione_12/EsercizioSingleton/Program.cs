aaa5deda6c216db502113f0273db6ef936a90f6a
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-27 17:41:44 +0200

    Add:
    +Esempi sul singleton
    +Esercizio 1 sul singleton
    +Esercizio 2 sul singleton

---
+++ Settimana_3/Lezione_12/EsercizioSingleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public sealed class Logger
+{
+    private static Logger _instance;
+
+    private Logger() { }
+
+    public static Logger GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Logger();
+        }
+        return _instance;
+    }
+
+    public void ScriviMessaggio(string messaggio)
+    {
+        Console.WriteLine($"{DateTime.Now} {messaggio}");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Logger log1 = Logger.GetInstance();
+        Logger log2 = Logger.GetInstance();
+
+        log1.ScriviMessaggio("Forza Napoli");
+        log2.ScriviMessaggio("Abbiamo vinto lo scudetto");
+
+        Console.WriteLine(log2 == log1);
+        Console.WriteLine(log1.GetHashCode() == log2.GetHashCode());
+    }
+}
---
+++ Settimana_3/Lezione_12/EsercizioSingletonLogger/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public sealed class Logger
+{
+    private static Logger _instance;
+
+    private List<string> listaLog;
+    private string _utente, _password;
+
+    private Logger()
+    {
+        listaLog = new List<string>();
+    }
+
+    public static Logger GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Logger();
+        }
+        return _instance;
+    }
+
+    public void CreaUtEPsw(string utente, string password)
+    {
+        _utente = utente;
+        _password = password;
+    }
+    public void Log(string message)
+    {
+        listaLog.Add(message);
+    }
+
+    public void MostraLog()
+    {
+        foreach (string m in listaLog)
+        {
+            Console.Write("-");
+            Console.WriteLine(m);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Logger log1 = Logger.GetInstance();
+
+        Console.WriteLine("Inserisci il nome utente:");
+        string utente = Console.ReadLine();
+
+        Console.WriteLine("Inserisci la password:");
+        string password = Console.ReadLine();
+        log1.CreaUtEPsw(utente, password);
+
+        Console.WriteLine("Inserisci il messaggio del log1: ");
+        log1.Log(Console.ReadLine());
+
+        Logger log2 = Logger.GetInstance();
+        Console.WriteLine("Inserisci messaggio del log2: ");
+        log2.Log(Console.ReadLine());
+
+        log1.MostraLog();
+        log2.MostraLog();
+
+        Console.WriteLine(log1 == log2);
+
+    }
+
+}
