626f43e969da60c5dace05d0d9b01049d157fb8c
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-29 17:37:17 +0200

    Add:
    +Esercizio Decorator Torte

---
+++ Settimana_3/Lezione_14/DecoratorTorte/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+public interface ITorta
+{
+    string Descrizione();
+}
+
+public class TortaCioccolato:ITorta
+{
+    public string Descrizione()
+    {
+        return "Torta al cioccolato ";
+    }
+}
+public class TortaVaniglia:ITorta
+{
+    public string Descrizione()
+    {
+        return "Torta alla vaniglia ";
+    }
+}
+public class TortaFrutta:ITorta
+{
+    public string Descrizione()
+    {
+        return "Torta alla frutta ";
+    }
+}
+
+public abstract class DecoratorTorte : ITorta
+{
+    protected ITorta baseTorta;
+
+    protected DecoratorTorte(ITorta b)
+    {
+        baseTorta = b;
+    }
+
+    public virtual string Descrizione()
+    {
+        return baseTorta.Descrizione();
+    }
+}
+
+public class ConPanna : DecoratorTorte
+{
+    public ConPanna(ITorta b) : base(b)
+    {
+
+    }
+
+    public override string Descrizione()
+    {
+        return base.Descrizione() + "+ panna ";
+    } 
+}
+
+public class ConFragole : DecoratorTorte
+{
+    public ConFragole(ITorta b) : base(b)
+    {
+
+    }
+
+    public override string Descrizione()
+    {
+        return base.Descrizione() + "+ fragole ";
+    }
+}
+
+public class ConGlassa : DecoratorTorte
+{
+    public ConGlassa(ITorta b) : base(b)
+    {
+
+    }
+
+    public override string Descrizione()
+    {
+        return base.Descrizione() + "+ glassa";
+    }
+}
+
+public static class TortaFactory
+{
+    public static ITorta CreaTortaBase(string tipo)
+    {
+        switch (tipo.ToLower())
+        {
+            case "cioccolato":
+                return new TortaCioccolato();
+            case "vaniglia":
+                return new TortaVaniglia();
+            case "frutta":
+                return new TortaFrutta();
+            default:
+                return null;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Inserisci la base torta vaniglia/cioccolato/frutta: ");
+        string tipo = Console.ReadLine();
+        var torta = TortaFactory.CreaTortaBase(tipo);
+
+        bool controllo = true;
+        do
+        {
+            Console.WriteLine("Cosa vuoi aggiungere panna,glassa,fragola o nulla: ");
+            string aggiunta = Console.ReadLine();
+
+            switch (aggiunta)
+            {
+                case "panna":
+                    torta = new ConPanna(torta);
+                    break;
+                case "glassa":
+                    torta = new ConGlassa(torta);
+                    break;
+                case "fragole":
+                    torta = new ConFragole(torta);
+                    break;
+                case "nulla":
+                    controllo = false;
+                    break;
+                default:
+                    Console.WriteLine("Scelta non valida");
+                    break;
+            }
+
+        } while (controllo);
+
+        Console.WriteLine(torta.Descrizione());
+    }
+}
+
