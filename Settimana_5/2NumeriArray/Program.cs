78d8f8ee5d9353512f98134c8939c67ea7cf3f62
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-06-09 13:18:07 +0200

    Add:
    Esercizio Leetcode

---
+++ Settimana_5/2NumeriArray/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Count(); i++)
+        {
+            for (int j = 1; j < nums.Count(); j++)
+            {
+                if (nums[i] + nums[j] == target && i != j)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { };
+    }
+}
